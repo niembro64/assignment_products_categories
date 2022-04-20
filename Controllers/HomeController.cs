56c6c9fac361474e25487b5c62bca21ad1c10c8e
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-04-20 13:51:54 -0400

    pre model INIT

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using assignment_products_categories.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace assignment_products_categories.Controllers
+{
+  public class HomeController : Controller
+  {
+    private MyContext _context;
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger, MyContext context)
+    {
+      _logger = logger;
+      _context = context;
+    }
+
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+  }
+}
